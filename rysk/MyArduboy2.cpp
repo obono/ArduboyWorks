7d9334a036a0eec02827a5e1c6b3c36f14b21cc1
OBONO <obono@users.noreply.github.com>
2020-03-03 22:59:14 +0900

    'Rysk' ver 0.10
    :+1: Use MyArduboy2 Library
    :smile: Tune easier

--- rysk/MyArduboy2.cpp
+++ rysk/MyArduboy2.cpp
@@ -1,4 +1,6 @@
-#include "MyArduboy.h"
+#include "MyArduboy2.h"
+
+/*  Local Variables  */
 
 PROGMEM static const uint32_t imgFont[] = {
     0x00000000, 0x00017000, 0x000C00C0, 0x0A7CA7CA, 0x0855F542, 0x19484253, 0x1251F55E, 0x00003000,
@@ -11,82 +13,26 @@ PROGMEM static const uint32_t imgFont[] = {
     0x1D184317, 0x0109C107, 0x114D5651, 0x0045F000, 0x0001F000, 0x0001F440, 0x000C1080, 0x10410410,
 };
 
-#ifdef USE_ARDUBOY2_LIB
+/*---------------------------------------------------------------------------*/
+/*                                 Initialize                                */
+/*---------------------------------------------------------------------------*/
 
-#define myAudio     audio
-
-MyArduboy::MyArduboy(void)
-{
-    pTunes = new ArduboyPlaytune(myAudio.enabled);
-    pTunes->initChannel(PIN_SPEAKER_1);
-}
-
-void MyArduboy::beginNoLogo(void)
+void MyArduboy2::beginNoLogo(void)
 {
     boot();
-    blank();
+    display();
     flashlight();
     systemButtons();
-    myAudio.begin();
+    audio.begin();
 }
 
-#else
+/*---------------------------------------------------------------------------*/
+/*                               Buttons status                              */
+/*---------------------------------------------------------------------------*/
 
-#define textSize    textsize
-#define textWrap    wrap
-#define pTunes      (&tunes)
-
-void MyArduboyAudio::begin()
+bool MyArduboy2::nextFrame(void)
 {
-    if (EEPROM.read(EEPROM_AUDIO_ON_OFF))
-        on();
-    else
-        off();
-}
-
-void MyArduboyAudio::on()
-{
-    power_timer3_enable();
-    audio_enabled = true;
-}
-
-void MyArduboyAudio::off()
-{
-    audio_enabled = false;
-    power_timer3_disable();
-}
-
-void MyArduboyAudio::toggle()
-{
-    if (audio_enabled)
-        off();
-    else
-        on();
-}
-
-void MyArduboy::beginNoLogo(void)
-{
-    boot();
-    if (pressed(UP_BUTTON)) {
-        sendLCDCommand(OLED_ALL_PIXELS_ON);
-        setRGBled(255, 255, 255);
-        power_timer0_disable();
-        while (true) {
-            idle(); // infinite loop
-        }
-    }
-    pTunes->initChannel(PIN_SPEAKER_1);
-    pinMode(PIN_SPEAKER_2, OUTPUT); // trick
-    myAudio.begin();
-}
-
-#endif
-
-/*----------------------------------------------------------------------------*/
-
-bool MyArduboy::nextFrame(void)
-{
-    bool ret = ARDUBOY_LIB_CLASS::nextFrame();
+    bool ret = Arduboy2::nextFrame();
     if (ret) {
         lastButtonState = currentButtonState;
         currentButtonState = buttonsState();
@@ -94,61 +40,45 @@ bool MyArduboy::nextFrame(void)
     return ret;
 }
 
-bool MyArduboy::buttonDown(uint8_t buttons)
+bool MyArduboy2::buttonDown(uint8_t buttons)
 {
     return currentButtonState & ~lastButtonState & buttons;
 }
 
-bool MyArduboy::buttonPressed(uint8_t buttons)
+bool MyArduboy2::buttonPressed(uint8_t buttons)
 {
     return currentButtonState & buttons;
 }
 
-
-bool MyArduboy::buttonUp(uint8_t buttons)
+bool MyArduboy2::buttonUp(uint8_t buttons)
 {
     return ~currentButtonState & lastButtonState & buttons;
 }
 
-/*----------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/*                                Draw letters                               */
+/*---------------------------------------------------------------------------*/
 
-void MyArduboy::setTextColor(uint8_t color)
+void MyArduboy2::setTextColors(uint8_t fgColor, uint8_t bgColor)
 {
-    setTextColor(color, (color == BLACK) ? WHITE : BLACK);
+    textColor = fgColor;
+    textBackground = bgColor;
 }
 
-void MyArduboy::setTextColor(uint8_t color, uint8_t bg)
-{
-    textcolor = color;
-    textbg = bg;
-}
-
-size_t MyArduboy::printEx(int16_t x, int16_t y, const char *p)
-{
-    setCursor(x, y);
-    return print(p);
-}
-
-size_t MyArduboy::printEx(int16_t x, int16_t y, const __FlashStringHelper *p)
-{
-    setCursor(x, y);
-    return print(p);
-}
-
-size_t MyArduboy::write(uint8_t c)
+size_t MyArduboy2::write(uint8_t c)
 {
     if (c == '\n') {
         cursor_y += textSize * 6;
         cursor_x = 0;
     } else if (c >= ' ' && c <= '_') {
-        myDrawChar(cursor_x, cursor_y, c, textcolor, textbg, textSize);
+        drawChar(cursor_x, cursor_y, c, textColor, textBackground, textSize);
         cursor_x += textSize * 6;
         if (textWrap && (cursor_x > (WIDTH - textSize * 6))) write('\n');
     }
     return 1; // temporary
 }
 
-void MyArduboy::myDrawChar(int16_t x, int16_t y, unsigned char c, uint8_t color, uint8_t bg, uint8_t size)
+void MyArduboy2::drawChar(int16_t x, int16_t y, unsigned char c, uint8_t color, uint8_t bg, uint8_t size)
 {
     bool draw_bg = bg != color;
 
@@ -177,17 +107,19 @@ void MyArduboy::myDrawChar(int16_t x, int16_t y, unsigned char c, uint8_t color,
     }
 }
 
-/*----------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/*                                Draw shapes                                */
+/*---------------------------------------------------------------------------*/
 
-void MyArduboy::drawRect2(int16_t x, int16_t y, uint8_t w, int8_t h, uint8_t color)
+void MyArduboy2::drawRect(int16_t x, int16_t y, uint8_t w, int8_t h, uint8_t color)
 {
-    drawFastHLine2(x, y, w, color);
-    drawFastHLine2(x, y + h - 1, w, color);
-    drawFastVLine2(x, y + 1, h - 2, color);
-    drawFastVLine2(x + w - 1, y + 1, h - 2, color);
+    drawFastHLine(x, y, w, color);
+    drawFastHLine(x, y + h - 1, w, color);
+    drawFastVLine(x, y + 1, h - 2, color);
+    drawFastVLine(x + w - 1, y + 1, h - 2, color);
 }
 
-void MyArduboy::drawFastVLine2(int16_t x, int16_t y, int8_t h, uint8_t color)
+void MyArduboy2::drawFastVLine(int16_t x, int16_t y, int8_t h, uint8_t color)
 {
     /*  Check parameters  */
     if (y < 0) {
@@ -203,7 +135,7 @@ void MyArduboy::drawFastVLine2(int16_t x, int16_t y, int8_t h, uint8_t color)
     uint8_t d = 0xFF << yOdd;
     y -= yOdd;
     h += yOdd;
-    for (buffer_t *p = getBuffer() + x + (y / 8) * WIDTH; h > 0; h -= 8, p += WIDTH) {
+    for (uint8_t *p = getBuffer() + x + (y / 8) * WIDTH; h > 0; h -= 8, p += WIDTH) {
         if (h < 8) d &= 0xFF >> (8 - h);
         if (color == BLACK) {
             *p &= ~d;
@@ -214,7 +146,7 @@ void MyArduboy::drawFastVLine2(int16_t x, int16_t y, int8_t h, uint8_t color)
     }
 }
 
-void MyArduboy::drawFastHLine2(int16_t x, int16_t y, uint8_t w, uint8_t color)
+void MyArduboy2::drawFastHLine(int16_t x, int16_t y, uint8_t w, uint8_t color)
 {
     /*  Check parameters  */
     if (x < 0) {
@@ -228,7 +160,7 @@ void MyArduboy::drawFastHLine2(int16_t x, int16_t y, uint8_t w, uint8_t color)
     /*  Draw a horizontal line  */
     uint8_t yOdd = y & 7;
     uint8_t d = 1 << yOdd;
-    buffer_t *p = getBuffer() + x + (y / 8) * WIDTH;
+    uint8_t *p = getBuffer() + x + (y / 8) * WIDTH;
     if (color == BLACK) {
         fillBeltBlack(p, d, w);
     } else {
@@ -236,7 +168,7 @@ void MyArduboy::drawFastHLine2(int16_t x, int16_t y, uint8_t w, uint8_t color)
     }
 }
 
-void MyArduboy::fillRect2(int16_t x, int16_t y, uint8_t w, int8_t h, uint8_t color)
+void MyArduboy2::fillRect(int16_t x, int16_t y, uint8_t w, int8_t h, uint8_t color)
 {
     /*  Check parameters  */
     if (x < 0) {
@@ -258,7 +190,7 @@ void MyArduboy::fillRect2(int16_t x, int16_t y, uint8_t w, int8_t h, uint8_t col
     uint8_t d = 0xFF << yOdd;
     y -= yOdd;
     h += yOdd;
-    for (buffer_t *p = getBuffer() + x + (y / 8) * WIDTH; h > 0; h -= 8, p += WIDTH) {
+    for (uint8_t *p = getBuffer() + x + (y / 8) * WIDTH; h > 0; h -= 8, p += WIDTH) {
         if (h < 8) d &= 0xFF >> (8 - h);
         if (color == BLACK) {
             fillBeltBlack(p, d, w);
@@ -269,7 +201,7 @@ void MyArduboy::fillRect2(int16_t x, int16_t y, uint8_t w, int8_t h, uint8_t col
     }
 }
 
-void MyArduboy::fillBeltBlack(buffer_t *p, uint8_t d, uint8_t w)
+void MyArduboy2::fillBeltBlack(uint8_t *p, uint8_t d, uint8_t w)
 {
     d = ~d;
     for (; w > 0; w--) {
@@ -277,47 +209,9 @@ void MyArduboy::fillBeltBlack(buffer_t *p, uint8_t d, uint8_t w)
     }
 }
 
-void MyArduboy::fillBeltWhite(buffer_t *p, uint8_t d, uint8_t w)
+void MyArduboy2::fillBeltWhite(uint8_t *p, uint8_t d, uint8_t w)
 {
     for (; w > 0; w--) {
         *p++ |= d;
     }
-}
-
-/*----------------------------------------------------------------------------*/
-
-bool MyArduboy::isAudioEnabled(void)
-{
-    return myAudio.enabled();
-}
-
-void MyArduboy::setAudioEnabled(bool on)
-{
-    if (on) {
-        myAudio.on();
-    } else {
-        myAudio.off();
-    }
-
-}
-
-void MyArduboy::saveAudioOnOff(void)
-{
-    myAudio.saveOnOff();
-}
-
-void MyArduboy::playScore2(const byte *score, uint8_t priority)
-{
-    if (!isAudioEnabled()) return;
-    if (pTunes->playing()) {
-        if (priority > playScorePriority) return;
-        pTunes->stopScore();
-    }
-    playScorePriority = priority;
-    pTunes->playScore(score);
-}
-
-void MyArduboy::stopScore2(void)
-{
-    pTunes->stopScore();
 }
